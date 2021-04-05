c74347c19949251ebf7a6a4690da21f6adf61ad5
ericason <erica.son25@gmail.com>
2021-04-05 15:36:47 +1000

    Added type interfaces

--- client/src/types/ABSSubmission.ts
+++
@@ -1,7 +0,0 @@
-export interface ABSSubmission {
-    periodOfObs_to: Date,
-    periodOfObs_from: Date,
-    obsEnv: string,
-    //examinerInitials: string, //is this needed for ABS
-    answers: number[]
-}
