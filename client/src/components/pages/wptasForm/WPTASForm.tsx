786aaf1335ae235622b14e20c4a74b01d02490ad
ericason <erica.son25@gmail.com>
2021-05-05 16:29:07 +1000

    Create WPTASQuestion file

--- client/src/components/pages/wptasForm/WPTASForm.tsx
+++ client/src/components/pages/wptasForm/WPTASForm.tsx
@@ -83,7 +83,6 @@ const useStyles = makeStyles((theme: Theme) =>
       height: 'fit-content',
       margin: 'auto',
       padding: 'auto',
-      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
       transform: 'translateZ(0)',
     },
     addMultiChoiceButton: {
