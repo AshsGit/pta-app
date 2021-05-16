f6feb5c40c2f89284a423c99d0746f6b32ba980e
ericason <erica.son25@gmail.com>
2021-05-16 17:08:07 +1000

    Deployment preparation

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const abs = require('./routes/api/abs');
 const wptas = require('./routes/api/wptas');
 const patients = require('./routes/api/patients');
+const path = require('path');
 
 const app = express();
 
@@ -30,6 +31,16 @@ mongoose
 app.use('/api/patient', patients);
 app.use('/api/wptas', wptas);
 app.use('/api/abs', abs);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
