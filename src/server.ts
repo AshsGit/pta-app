cff4349a3b3c2b001a5bc1e5dd0e1e8022b9f610
ericason <erica.son25@gmail.com>
2021-05-16 17:27:28 +1000

    Client side routing fix

--- src/server.ts
+++ src/server.ts
@@ -38,10 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    let url = path.join(__dirname, 'client', 'build', 'index.html');
-    if (!url.startsWith('/app/')) url = url.substring(1);
-    res.sendFile(url);
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
