489bf02812323f115ef9762b7ef15c00be0f5838
ericason <erica.son25@gmail.com>
2021-05-16 17:38:46 +1000

    Client side routing fix

--- src/server.ts
+++ src/server.ts
@@ -37,8 +37,9 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
+  // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
